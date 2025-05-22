ea41bda776df6eb5f68eeb8a9c9dc6f977cdf7cf
GeonHui Kim <84655228+Geon-hui@users.noreply.github.com>
2025-05-22 22:15:59 +0900

    Update generate_audio.py
    path+ prompt 수정

--- script/inference/generate_audio.py
+++ script/inference/generate_audio.py
@@ -1,48 +1,33 @@
+
 import os
-import sys
 import torch
+import soundfile as sf
 from diffusers import AudioLDMPipeline, UNet2DConditionModel
 from safetensors.torch import load_file
-import soundfile as sf
 
 def main():
-    # 기본 모델 로드
     base_model_id = "cvssp/audioldm-s-full-v2"
+    lora_weights_path = "/home/2019111986/AudioLDM-LoRA-unet/AudioLDM-with-LoRA/data/LoRA_weight/checkpoint-20000/model.safetensors"
+    prompt = "This instrumental track blends lively flute melodies together with punchy drums, delivering a unique listening experience that captivates the ear without the need for vocals. The subgenre of hip-hop is boom bap."
+
+    checkpoint_name = os.path.basename(os.path.dirname(lora_weights_path))
+    output_dir = f"./AudioLDM-with-LoRA/generated_audio/{checkpoint_name}"
+    os.makedirs(output_dir, exist_ok=True)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
     pipe = AudioLDMPipeline.from_pretrained(base_model_id)
-    
-    # LoRA 가중치 경로
-    lora_weights_path = "/home/2020112030/WorkSpace/2025/AudioLDM-with-LoRA/data/LoRA_weight/checkpoint-97000/model.safetensors"
-    
-    # LoRA 가중치 로드
     unet = UNet2DConditionModel.from_pretrained(base_model_id, subfolder="unet")
     state_dict = load_file(lora_weights_path)
     unet.load_state_dict(state_dict, strict=False)
-    
-    # 수정된 UNet을 파이프라인에 적용
     pipe.unet = unet
-    
-    # GPU로 이동
-    device = "cuda" if torch.cuda.is_available() else "cpu"
     pipe = pipe.to(device)
-    
-    # 생성할 프롬프트 설정
-    prompt = "hip hop music, The subgenre of hip-hop is boom bap."
-    
-    # 오디오 생성
-    audio = pipe(
-        prompt=prompt,
-        num_inference_steps=50,
-        audio_length_in_s=4.0,
-        guidance_scale=7.5,
-    ).audios[0]
-    
-    # 결과 저장
-    output_dir = "./generated_audio"
-    os.makedirs(output_dir, exist_ok=True)
-    output_path = os.path.join(output_dir, prompt + "_97000_r2_alpha4_003.wav")
-    sf.write(output_path, audio, samplerate=16000)
-    
-    print(f"Generated audio saved to: {output_path}")
+
+    for i in range(3):
+        audio = pipe(prompt=prompt, num_inference_steps=50, audio_length_in_s=4.0, guidance_scale=7.5).audios[0]
+        output_path = os.path.join(output_dir, f"gen_{i}.wav")
+        sf.write(output_path, audio, samplerate=16000)
+        print(f"[✓] gen_{i}.wav saved")
 
 if __name__ == "__main__":
-    main() 
+    main()
